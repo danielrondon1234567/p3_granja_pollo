10c3a7cc9fa93fcb1be9b563262f4d32ad9ecc31
danielrondon1234567 <danielalejandro35.m@gmail.com>
2025-05-08 12:07:49 -0400

    Create Base_datos.py

---
+++ Base_datos.py
@@ -0,0 +1,9 @@
+class base_datos:
+  def_init_(self):
+  self.lista_datos=[["P001","Pequeña","15dias"], ["P002","Grande","5dias"]]
+
+def imprimir_info(self):
+  for self.lista_datos in range(self.lista_datos.len):
+    for(info_pollo)
+
+

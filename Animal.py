1a7022edae8a6becdcfb0d4a45ac237ea3e4a2d7
Juanes0102 <fazejuanescruz@gmail.com>
2025-05-15 00:05:20 -0500

    Update Animal.py

--- Animal.py
+++ Animal.py
@@ -1,2 +1,8 @@
+class Animal:
+    def _init_(self, codigo, raza, edad):
+        self.codigo = codigo
+        self.raza = raza
+        self.edad = edad
 
-   
+    def _str_(self):
+        return f"Animal[código={self.codigo}, raza={self.raza}, edad={self.edad}]"

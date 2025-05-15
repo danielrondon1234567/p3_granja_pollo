31041df10d6f7e1518379cf89d504b4264cc2899
Juanes0102 <fazejuanescruz@gmail.com>
2025-05-15 00:19:37 -0500

    Update Base_datos.py

--- Base_datos.py
+++ Base_datos.py
@@ -1,34 +1,49 @@
-class base_datos(CrudAnimal):
-  def_init_(self):
-  self.lista_datos=[]
-  self.producciones=[]
+from crud_animal import CrudAnimal
+from produccion import Produccion
 
-def crear_animal(self, animal):
-  self.aniamles[animal.codigo]=animal
-  self.producciones[animal.codigo]=Produccion(animal.codigo)
+class BaseDatos(CrudAnimal):
+    def __init__(self):
+        self.animales = {}
+        self.producciones = {}
 
-def leer_aniaml(self, codigo):
-  return self.animales.get(codigo)
+    def crear_animal(self, animal):
+        if animal.codigo in self.animales:
+            print(f"Ya existe un animal con el código {animal.codigo}")
+            return
+        self.animales[animal.codigo] = animal
+        self.producciones[animal.codigo] = Produccion(animal.codigo)
 
-def actualizar_animal(self, codigo, nuevo_animal):
-  if codigo in self.animales:
-      self.animales[codigo]=nuevo_animal
+    def leer_animal(self, codigo):
+        return self.animales.get(codigo)
 
-def eliminar_animal(self,codigo):
-  if codigo in self.animales:
-    del self.animales[codigo]
-    del self.produciones[codigo]
+    def actualizar_animal(self, codigo, nuevo_animal):
+        if codigo in self.animales:
+            self.animales[codigo] = nuevo_animal
+        else:
+            print(f"No se encontró el animal con código {codigo}")
 
-def regitrar_produccion(self,codigo,semana,cantidad)
-if codigo in self.producciones:
-  self.producciones[codigo].registrar_producion(semana, cantidad)
+    def eliminar_animal(self, codigo):
+        if codigo in self.animales:
+            del self.animales[codigo]
+            if codigo in self.producciones:
+                del self.producciones[codigo]
+        else:
+            print(f"No se encontró el animal con código {codigo}")
 
-def mostrar_producion(self,codigo):
-  if codigo in self.produciones:
-    print(self.producines[codigo])
-  else:
-    print("no hay produccion registrada para ese codigo")
+    def registrar_produccion(self, codigo, semana, cantidad):
+        if codigo in self.producciones:
+            self.producciones[codigo].registrar_produccion(semana, cantidad)
+        else:
+            print(f"No hay producción registrada para el código {codigo}")
 
-def reporte_general(self):
-  for codigo, produccion in self.producciones.items():
-    print(f"{codigo}-total huevos: {produccion.produccion_total()}")
+    def mostrar_produccion(self, codigo):
+        if codigo in self.producciones:
+            print(self.producciones[codigo])
+        else:
+            print("No hay producción registrada para ese código")
+
+    def reporte_general(self):
+        print("Reporte general de producción:")
+        for codigo, produccion in self.producciones.items():
+            print(f"{codigo} - Total huevos: {produccion.produccion_total()}")
+
